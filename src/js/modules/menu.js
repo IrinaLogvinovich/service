2e2e095c7dd13dfd023d97e0f3100e9613373fc3
dev16 <dev16@idea-labs.ru editor = "C:\Programs\Microsoft VS Code\Code.exe" --wait>
2020-02-20 09:54:57 +0500

    мобильное меню

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -5,3 +5,19 @@ $(".anchor").on("click", function () {
   var target = $(get_id).offset().top;
   $("html, body").animate({ scrollTop: target }, 800);
 });
+
+$(".header__burger").on('click', function(){
+  $('.header__menu').fadeToggle(400);
+
+  $('.header__menu').on('click', function(){
+    if($(document).width() <=1024) {
+      $(this).hide();
+    }
+  });
+});
+
+$(window).on('resize', function(){
+  if($(document).width() > 1024) {
+    $('.header__menu').show();
+  }
+})
